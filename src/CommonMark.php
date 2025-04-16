ccbdfe14f223a08c5282cf82fd776ba098fd9b03
roadiz-ci <ambroise+ci@roadiz.io>
2025-04-16 17:33:33 +0000

    chore: Bumped

--- src/CommonMark.php
+++ src/CommonMark.php
@@ -9,12 +9,27 @@ use Symfony\Component\Stopwatch\Stopwatch;
 
 final class CommonMark implements MarkdownInterface
 {
+    private ?Stopwatch $stopwatch;
+    private MarkdownConverter $textConverter;
+    private MarkdownConverter $lineConverter;
+    private MarkdownConverter $textExtraConverter;
+
+    /**
+     * @param MarkdownConverter $textConverter
+     * @param MarkdownConverter $textExtraConverter
+     * @param MarkdownConverter $lineConverter
+     * @param Stopwatch|null      $stopwatch
+     */
     public function __construct(
-        private readonly MarkdownConverter $textConverter,
-        private readonly MarkdownConverter $textExtraConverter,
-        private readonly MarkdownConverter $lineConverter,
-        private readonly ?Stopwatch $stopwatch = null
+        MarkdownConverter $textConverter,
+        MarkdownConverter $textExtraConverter,
+        MarkdownConverter $lineConverter,
+        ?Stopwatch $stopwatch = null
     ) {
+        $this->textConverter = $textConverter;
+        $this->textExtraConverter = $textExtraConverter;
+        $this->lineConverter = $lineConverter;
+        $this->stopwatch = $stopwatch;
     }
 
     public function text(string $markdown = null): string
@@ -22,9 +37,13 @@ final class CommonMark implements MarkdownInterface
         if (null === $markdown) {
             return '';
         }
-        $this->stopwatch?->start(CommonMark::class . '::text');
+        if (null !== $this->stopwatch) {
+            $this->stopwatch->start(CommonMark::class . '::text');
+        }
         $html = $this->textConverter->convert($markdown)->getContent();
-        $this->stopwatch?->stop(CommonMark::class . '::text');
+        if (null !== $this->stopwatch) {
+            $this->stopwatch->stop(CommonMark::class . '::text');
+        }
         return $html;
     }
 
@@ -33,9 +52,13 @@ final class CommonMark implements MarkdownInterface
         if (null === $markdown) {
             return '';
         }
-        $this->stopwatch?->start(CommonMark::class . '::textExtra');
+        if (null !== $this->stopwatch) {
+            $this->stopwatch->start(CommonMark::class . '::textExtra');
+        }
         $html = $this->textExtraConverter->convert($markdown)->getContent();
-        $this->stopwatch?->stop(CommonMark::class . '::textExtra');
+        if (null !== $this->stopwatch) {
+            $this->stopwatch->stop(CommonMark::class . '::textExtra');
+        }
         return $html;
     }
 
@@ -44,9 +67,13 @@ final class CommonMark implements MarkdownInterface
         if (null === $markdown) {
             return '';
         }
-        $this->stopwatch?->start(CommonMark::class . '::line');
+        if (null !== $this->stopwatch) {
+            $this->stopwatch->start(CommonMark::class . '::line');
+        }
         $html = $this->lineConverter->convert($markdown)->getContent();
-        $this->stopwatch?->stop(CommonMark::class . '::line');
+        if (null !== $this->stopwatch) {
+            $this->stopwatch->stop(CommonMark::class . '::line');
+        }
         return $html;
     }
 }
