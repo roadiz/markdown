32e29e548099dc982f4372f9146cc49d41729ae5
roadiz-ci <ambroise+ci@roadiz.io>
2025-09-10 16:52:50 +0000

    Merge branch hotfix/v2.6.8

--- src/Twig/MarkdownExtension.php
+++ src/Twig/MarkdownExtension.php
@@ -14,53 +14,42 @@ final class MarkdownExtension extends AbstractExtension
     {
     }
 
+    #[\Override]
     public function getFilters(): array
     {
         return [
-            new TwigFilter('markdown', [$this, 'markdown'], ['is_safe' => ['html']]),
-            new TwigFilter('inlineMarkdown', [$this, 'inlineMarkdown'], ['is_safe' => ['html']]),
-            new TwigFilter('inline_markdown', [$this, 'inlineMarkdown'], ['is_safe' => ['html']]),
-            new TwigFilter('markdownExtra', [$this, 'markdownExtra'], ['is_safe' => ['html']]),
-            new TwigFilter('markdown_extra', [$this, 'markdownExtra'], ['is_safe' => ['html']]),
+            new TwigFilter('markdown', $this->markdown(...), ['is_safe' => ['html']]),
+            new TwigFilter('inlineMarkdown', $this->inlineMarkdown(...), ['is_safe' => ['html']]),
+            new TwigFilter('inline_markdown', $this->inlineMarkdown(...), ['is_safe' => ['html']]),
+            new TwigFilter('markdownExtra', $this->markdownExtra(...), ['is_safe' => ['html']]),
+            new TwigFilter('markdown_extra', $this->markdownExtra(...), ['is_safe' => ['html']]),
         ];
     }
 
-    /**
-     * @param string|null $input
-     *
-     * @return string
-     */
     public function markdown(?string $input): string
     {
         if (null === $input) {
             return '';
         }
+
         return $this->markdown->text($input);
     }
 
-    /**
-     * @param string|null $input
-     *
-     * @return string
-     */
     public function inlineMarkdown(?string $input): string
     {
         if (null === $input) {
             return '';
         }
+
         return $this->markdown->line($input);
     }
 
-    /**
-     * @param string|null $input
-     *
-     * @return string
-     */
     public function markdownExtra(?string $input): string
     {
         if (null === $input) {
             return '';
         }
+
         return $this->markdown->textExtra($input);
     }
 }
