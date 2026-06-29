bd2d19c16927455cfa05f2766e22dcbd6d1eb447
roadiz-ci <ambroise+ci@roadiz.io>
2026-06-29 10:04:17 +0000

    Merge branch hotfix/v2.7.29 into main

--- src/CommonMark.php
+++ src/CommonMark.php
@@ -13,34 +13,41 @@ final readonly class CommonMark implements MarkdownInterface
         private MarkdownConverter $textConverter,
         private MarkdownConverter $textExtraConverter,
         private MarkdownConverter $lineConverter,
+        private MarkdownConverter $textHtmlConverter,
+        private MarkdownConverter $textExtraHtmlConverter,
         private ?Stopwatch $stopwatch = null,
     ) {
     }
 
-    public function text(?string $markdown = null): string
+    #[\Override]
+    public function text(?string $markdown = null, bool $allowHtml = false): string
     {
         if (null === $markdown) {
             return '';
         }
         $this->stopwatch?->start(CommonMark::class.'::text');
-        $html = $this->textConverter->convert($markdown)->getContent();
+        $converter = $allowHtml ? $this->textHtmlConverter : $this->textConverter;
+        $html = $converter->convert($markdown)->getContent();
         $this->stopwatch?->stop(CommonMark::class.'::text');
 
         return $html;
     }
 
-    public function textExtra(?string $markdown = null): string
+    #[\Override]
+    public function textExtra(?string $markdown = null, bool $allowHtml = false): string
     {
         if (null === $markdown) {
             return '';
         }
         $this->stopwatch?->start(CommonMark::class.'::textExtra');
-        $html = $this->textExtraConverter->convert($markdown)->getContent();
+        $converter = $allowHtml ? $this->textExtraHtmlConverter : $this->textExtraConverter;
+        $html = $converter->convert($markdown)->getContent();
         $this->stopwatch?->stop(CommonMark::class.'::textExtra');
 
         return $html;
     }
 
+    #[\Override]
     public function line(?string $markdown = null): string
     {
         if (null === $markdown) {
